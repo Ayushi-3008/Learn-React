7e0026a008f358b331c53f591694f32d9d579e95
Ayushi sinha <ayushi.s@mamaearth.in>
2024-05-09 03:35:43 +0530

    React component and project run setup

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I am an H1 tag "),
-        React.createElement("h2", {}, "I am an H2 tag")]))
+// React.createElement => ReactDOM(Object) => HTML Element
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel
+// JSX => React.createElement => ReactDOM(Object) => HTML Element
+const elem = <span>React Element</span>
+
+const heading = (
+    <h1 className="heading" tabIndex="5">
+        {elem}
+        Learn React using JSX
+    </h1>
+)
+
+// React Functional Component
+const HeadingComponent = () => (
+    <div id="container">
+        {heading}
+        <h1 id="heading">React functional component</h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />)
